8f6f13a5aa20e6e66055026be62b58cc105d68c9
Олег Малинский <o.malinsky@infoflot.ru>
2019-04-12 13:19:29 +0300

    Singleton

---
+++ Singleton.php
@@ -0,0 +1,45 @@
+<?php
+
+class Singleton {
+
+    private static $instance = [];
+
+    protected function __construct() {}
+
+    private function __clone() {}
+
+    private function __wakeup() {
+        throw new Exception("Cannot use __wakeup method in singleton class");
+    }
+
+    public static function getInstance() {
+
+        $class = static::class;
+
+        if (!isset(self::$instance[$class])) {
+            self::$instance[$class] = new static;
+        }
+
+        return self::$instance[$class];
+    }
+
+}
+
+function clientCode() {
+
+    $std = Singleton::getInstance();
+    $std->opt1 = 'opt1';
+    $std1 = Singleton::getInstance();
+    $std1->opt2 = 'opt2';
+
+    if ($std === $std1) {
+        print_r($std);
+        print_r($std1);
+    } else {
+        echo 'ERROR';
+    }
+}
+
+clientCode();
+
+?>
