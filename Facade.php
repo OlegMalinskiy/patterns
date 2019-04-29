420dd71a2c22fea93e1c49fc05c8bb99035a4d43
Олег Малинский <o.malinsky@infoflot.ru>
2019-04-29 13:18:22 +0300

    Facade

---
+++ Facade.php
@@ -0,0 +1,61 @@
+<?php
+
+class Facade {
+
+	protected $firstLibrary;
+	protected $secondLibrary;
+
+	public function __construct() {
+
+		$this->firstLibrary = new FirstLibraryClass;
+		$this->secondLibrary = new SecondLibraryClass;
+	}
+
+	public function operation() {
+
+		$result = '';
+
+		$result .= $this->firstLibrary->doSomeOperation();
+		$result .= $this->secondLibrary->doMoreOperation();
+		$result .= '<p>Add another functionality</p>';
+		$result .= $this->firstLibrary->doAnotherOperation();
+		$result .= $this->secondLibrary->doOperation();
+
+		return $result;
+	}
+}
+
+
+
+class FirstLibraryClass {
+
+	public function doSomeOperation(): string {
+		return '<p>Do some operation from FirstLibraryClass</p>';
+	}
+
+	public function doAnotherOperation(): string {
+		return '<p>Do another operation from FirstLibraryClass</p>';
+	}
+}
+
+class SecondLibraryClass {
+
+	public function doOperation(): string {
+		return '<p>Do operation from SecondLibraryClass</p>';
+	}
+
+	public function doMoreOperation(): string {
+		return '<p>Do more operation from SecondLibraryClass</p>';
+	}
+}
+
+
+function clientCode(Facade $facade) {
+
+	echo $facade->operation();
+}
+
+clientCode(new Facade);
+
+
+?>
