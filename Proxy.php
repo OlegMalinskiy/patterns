f14b18a913d0a7df38e08a4f68e2bfb290826075
Олег Малинский <o.malinsky@infoflot.ru>
2019-05-14 17:44:43 +0300

    Proxy

---
+++ Proxy.php
@@ -0,0 +1,57 @@
+<?php
+
+interface Receiver {
+	public function getInfo(int $id): string;
+}
+
+class SimpleReceiver implements Receiver {
+
+	public function getInfo(int $id): string {
+
+		return "<p>Some information from SimpleReceiver with id = {$id}</p>";
+	}
+}
+
+class CacheReceiver implements Receiver {
+
+	private $simple;
+
+	private $cache = [];
+
+	public function __construct(SimpleReceiver $simple) {
+
+		$this->simple = $simple;
+	}
+
+	public function getInfo(int $id): string {
+
+		if (!isset($this->cache[$id])) {
+			
+			$info = $this->simple->getInfo($id);
+			$this->cache[$id] = $info;
+			return $info;
+		} else {
+
+			$info = sprintf("<hr>From cache! %s<hr>", $this->cache[$id]);
+			return $info;
+		}
+
+	}
+}
+
+function clientCode(Receiver $receiver) {
+
+	echo $receiver->getInfo(2);
+	echo $receiver->getInfo(3);
+	echo $receiver->getInfo(2);
+	echo $receiver->getInfo(3);
+	echo $receiver->getInfo(2);
+	echo $receiver->getInfo(1);
+}
+
+$simple = new SimpleReceiver;
+
+clientCode($simple);
+clientCode(new CacheReceiver($simple));
+
+?>
