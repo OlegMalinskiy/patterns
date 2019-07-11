00e9ea56aedf8691f269a08c6b3e329811c52ed5
Олег Малинский <o.malinsky@infoflot.ru>
2019-07-11 10:41:25 +0300

    TemplateMethod

---
+++ TemplateMethod.php
@@ -0,0 +1,61 @@
+<?php
+
+abstract class Base {
+
+	public function execute(): void {
+
+		$this->firstMethod();
+
+		$this->baseMethod();
+
+		$this->secondMethod();
+	}
+
+	public function baseMethod(): void {
+
+		echo '<p>' . get_class($this) . ': call baseMethod</p>'; 
+	}
+
+	abstract function firstMethod(): void;
+
+	abstract function secondMethod(): void;
+}
+
+class FirstClass extends Base {
+
+	public function firstMethod(): void {
+
+		echo '<p>Call FirstClass::firstMethod<p>';
+	}
+
+	public function secondMethod(): void {
+
+		echo '<p>Call FirstClass::secondMethod</p>';
+	}
+}
+
+class SecondClass extends Base {
+
+	public function firstMethod(): void {
+
+		echo '<p>Call SecondClass::firstMethod</p>';
+	}
+
+	public function secondMethod(): void {
+
+		echo '<p>Call SecondClass::secondMethod</p>';
+	}
+}
+
+function client(Base $base) {
+
+	$base->execute();
+}
+
+client(new FirstClass);
+
+echo '<hr>';
+
+client(new SecondClass);
+
+?>
